6c532f4d67ff9792a78c4cf455e4dd8d16d82eef
arda-ornek <arda.ornek@gmail.com>
2022-09-18 14:51:07 +0300

    pass props to components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "../src/data";
 import "../src/style.css"
+
 export default function App() {
-  return(
-    <div>
-      <Navbar/>
-      <Hero/>
-      <Card img="katie-zaferes.png" 
-      rating="5.0" 
-      reviewCount={6} 
-      country="USA" 
-      title="Life Lessons with Katie Zaferes" 
-      price={136}/>
-    </div>
-    
+  const cards = data.map(item => {
+  return (
+    <Card 
+        key={item.id}
+        item={item}
+    />
+  )
+  })        
+
+  return (
+  <div>
+    <Navbar />
+    <Hero/>
+    <section className="cards-list">
+      {cards}
+    </section>
+  </div>
   )
 }
